2f479819b92d72718862b1abb499a05026179606
中島浩二 <knaka0209@gmail.com>
2024-08-18 11:58:42 +0900

    up, 0818a9

--- src/pages/renderLayout.ts
+++ src/pages/renderLayout.ts
@@ -1,11 +1,20 @@
 
+let PATH__FAVICON = "/favicon.ico";
+let PATH__ENTRY_CLIENT = "/static/entry-client.js";
+//
 export default function Compo(props: any) {
+console.log("renderLayout.env.PROD=", import.meta.env.PROD);
+  if(import.meta.env.PROD) {
+    PATH__FAVICON = "/public/favicon.ico";
+    PATH__ENTRY_CLIENT = "/public/static/entry-client.js";
+  }
+  //
   const html = `<!DOCTYPE html><html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>${props.title}</title>
-    <link rel="icon" href="/favicon.ico" type="image/x-icon"></link>
+    <link rel="icon" href="${PATH__FAVICON}" type="image/x-icon"></link>
     <script
     src="https://unpkg.com/htmx.org@1.9.10"
     integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC"
@@ -22,7 +31,7 @@ export default function Compo(props: any) {
     </div>
     <hr />        
     ${props.children}
-    <script type="module" src="/static/entry-client.js"></script>
+    <script type="module" src="${PATH__ENTRY_CLIENT}"></script>
   </body></html>
   `
   return html;
